9d47535a016f19ab5cd32bae22866e2e21321a54
Yash Brahmankar <yashbrahmankar95@gmail.com>
2026-01-01 23:14:21 +0530

    first commit

---
+++ 2206_Divide_Array_Into_Equal_Pairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def divideArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else :
+                d[i] += 1
+            
+        for j in d.values():
+            if j % 2 != 0:
+                return False
+        return True 
+
+
+
+
