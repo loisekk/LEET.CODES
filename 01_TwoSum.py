66f701d4621504144e0beb237ea1a58bfe1ffc79
YASH BRAHMANKAR <yashbrahmankar95@gmail.com>
2026-01-02 22:56:23 +0530

    Delete 01_TwoSum.py

--- 01_TwoSum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums , target):
-        prevmap = {}
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevmap:
-                return [prevmap[diff], i]
-            prevmap[n] = i
