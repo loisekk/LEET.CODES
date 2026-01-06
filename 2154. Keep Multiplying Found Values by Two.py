9d38c627fbb4dff6865db265fb2636a3650f92e2
YASH BRAHMANKAR <yashbrahmankar95@gmail.com>
2026-01-06 19:21:07 +0530

    Add files via upload

---
+++ 2154. Keep Multiplying Found Values by Two.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """
+        nums_set = set(nums)
+
+        while original in nums_set :
+            original *= 2
+        return original
