1576440a7df6d1b7b9dc46ba2f818e6aecb9a809
YASH BRAHMANKAR <yashbrahmankar95@gmail.com>
2026-01-11 21:29:40 +0530

    Add files via upload

---
+++ 2160. Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minimumSum(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        num1 = num
+        num2 = (num , '|')
+        while True :
+            if num == (num1) and num ==(num2):
+               [num1] < [num2] or [num2] < [num1]
+            
+        return num1 , num2
+
